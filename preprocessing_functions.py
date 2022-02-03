cc8c4bdf49218fe999b94dd026625a0783be962a
zarmi <ursinozarmina@gmail.com>
2022-02-03 13:56:00 +0100

    updated with new function

--- preprocessing_functions.py
+++ preprocessing_functions.py
@@ -1,3 +1,12 @@
+import string
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+
+
 def emojis_replacement(col):
   for index, i in enumerate(col):
     i=str(i).replace("�", "")
@@ -11,3 +20,41 @@ def emojis_replacement(col):
 def lower_column(column):
   for col in [column]:
     df[col]=df[col].str.lower()
+    
+PUNCT_TO_REMOVE = string.punctuation
+def remove_punctuation(text):
+    """custom function to remove the punctuation"""
+    return text.translate(str.maketrans('', '', PUNCT_TO_REMOVE))
+
+STOPWORDS = set(stopwords.words('english'))
+def remove_stopwords(text):
+    """custom function to remove the stopwords"""
+    return " ".join([word for word in str(text).split() if word not in STOPWORDS])
+
+
+def remove_freqwords(text):
+    """custom function to remove the frequent words"""
+    return " ".join([word for word in str(text).split() if word not in FREQWORDS])
+
+def remove_rarewords(text):
+    """custom function to remove the rare words"""
+    return " ".join([word for word in str(text).split() if word not in RAREWORDS])
+
+
+stemmer = PorterStemmer()
+def stem_words(text):
+    return " ".join([stemmer.stem(word) for word in text.split()])
+
+
+lemmatizer = WordNetLemmatizer()
+def lemmatize_words(text):
+    return " ".join([lemmatizer.lemmatize(word) for word in text.split()])
+
+
+def remove_urls(text):
+    url_pattern = re.compile(r'https?://\S+|www\.\S+')
+    return url_pattern.sub(r'', text)
+
+def remove_html(text):
+    html_pattern = re.compile('<.*?>')
+    return html_pattern.sub(r'', text)
