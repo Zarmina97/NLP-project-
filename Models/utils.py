fbf0b9e95046166ba341096b2ce6ce9516727cf0
Jyoti Yadav <43088895+jyoti123yadav@users.noreply.github.com>
2022-05-03 16:49:46 +0200

    UPLOAD
    UPLOAD

---
+++ Models/utils.py
@@ -0,0 +1,114 @@
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+import seaborn as sns
+from sklearn import preprocessing
+from sklearn.metrics import r2_score
+from matplotlib import pyplot as plt
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import max_error
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import RandomizedSearchCV
+
+
+
+def hyperparameterTuning_RandomForest(x,y):
+      model_rand = RandomForestRegressor()
+      n_estimators = [5,20,50,100] # number of trees in the random forest
+      max_features = ['auto', 'sqrt'] # number of features in consideration at every split
+      max_depth = [int(x) for x in np.linspace(10, 120, num = 12)] # maximum number of levels allowed in each decision tree
+      min_samples_split = [2, 6, 10] # minimum sample number to split a node
+      min_samples_leaf = [1, 3, 4] # minimum sample number that can be stored in a leaf node
+      bootstrap = [True, False] # method used to sample data points
+      random_grid = {'n_estimators': n_estimators,
+      'max_features': max_features,
+      'max_depth': max_depth,
+      'min_samples_split': min_samples_split,
+      'min_samples_leaf': min_samples_leaf,
+      'bootstrap': bootstrap}
+      rf_random = RandomizedSearchCV(estimator = model_rand,param_distributions = random_grid,n_iter = 100, cv = 5, verbose=2, random_state=35, n_jobs = -1)
+      rf_random.fit(x, y)
+      return rf_random.best_params_
+
+
+def plot_feature_importance(importance,names,model_type):
+
+#Create arrays from feature importance and feature names
+  feature_importance = np.array(importance)
+  feature_names = np.array(names)
+
+  for i,v in enumerate(feature_importance):
+    print('Feature: %0d, Score: %.5f' % (i,v))
+
+  #Create a DataFrame using a Dictionary
+  data={'feature_names':feature_names,'feature_importance':feature_importance}
+  fi_df = pd.DataFrame(data)
+
+  #Sort the DataFrame in order decreasing feature importance
+  fi_df.sort_values(by=['feature_importance'], ascending=False,inplace=True)
+
+  #Define size of bar plot
+  plt.figure(figsize=(20,20))
+  #Plot Searborn bar chart
+  sns.barplot(x=fi_df['feature_importance'], y=fi_df['feature_names'])
+  #Add chart labels
+  plt.title(model_type + 'FEATURE IMPORTANCE')
+  plt.xlabel('FEATURE IMPORTANCE')
+  plt.ylabel('FEATURE NAMES')
+
+
+
+
+def metrics(actual,predicted):
+    rmse = np.sqrt(mean_squared_error(actual, predicted))
+    print("RMSE: %f" % (rmse))
+    print()
+
+    R2=r2_score(actual,predicted)
+    print("R2 Score: %f" % (R2))
+    print()
+
+    MAE=mean_absolute_error(actual,predicted)
+    print("MAE : %f" % (MAE))
+    print()
+
+    MSE=mean_squared_error(actual,predicted)
+    print("MSE : %f" % (MSE))
+    print()
+
+
+    RMSLE=np.log(np.sqrt(mean_squared_error(actual,predicted)))
+    print("RMSLE : %f" % (RMSLE))
+    print()
+
+    max_error_=max_error(actual,predicted)
+    print("max_error : %f" % (max_error_))
+    print()
+
+
+def hyperparameterTuning_XGBoost(x,y):
+    model = xgb.XGBRegressor()
+    parameters = {'nthread':[4],
+                'objective':['reg:squarederror'],
+                'learning_rate': [.03, 0.05, .07], #so called 'eta' value
+                'max_depth': [5, 6, 7],
+                'min_child_weight': [4],
+                'subsample': [0.7],
+                'colsample_bytree': [0.7],
+                'n_estimators': [500]}
+    model_grid = GridSearchCV(model,parameters,cv = 2,n_jobs = 5, verbose=True)
+    model_grid.fit(x, y)
+    model_grid.best_score_
+    model_grid.best_params_
+    bestScore=model_grid.best_score_
+    bestParams=model_grid.best_params_
+    return bestScore,bestParams
+
+
+
+def Normalizing(X):
+    normalizer = preprocessing.MinMaxScaler()
+    normalized = pd.DataFrame(normalizer.fit_transform(X),  columns = X.columns)
+    return normalized
