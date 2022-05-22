54252186be8ea1e09c0169dd1ed30e9290f2bd5d
Jyoti Yadav <43088895+jyoti123yadav@users.noreply.github.com>
2022-05-22 17:50:22 +0200

    updated
    updated

--- Models/utils.py
+++ Models/utils.py
@@ -170,6 +170,10 @@ def Feature_Extraction(dataframe,n,featureExtraction):
 
 
 
-
+def clean_dataset(df):
+    assert isinstance(df, pd.DataFrame), "df needs to be a pd.DataFrame"
+    df.dropna(inplace=True)
+    indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
+    return df[indices_to_keep].astype(np.float64)
 
 
